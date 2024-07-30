64b97af0458a0f221a367e4df98b345c7c58831c
obuzietter <obuyacalvince672@gmail.com>
2024-07-30 18:33:32 +0300

    MINI POS INITIAL COMMIT

---
+++ src/main/java/com/mycompany/minipos/MiniPOS.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.minipos;
+
+/**
+ *
+ * @author OBUZIETTER3000
+ */
+public class MiniPOS {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
