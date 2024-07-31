395ec602b691351e4137d8c09d0abc38b2d296d6
obuzietter <obuyacalvince672@gmail.com>
2024-07-31 21:31:51 +0300

    desktop pane

--- src/main/java/com/mycompany/minipos/User.java
+++ src/main/java/com/mycompany/minipos/User.java
@@ -1,19 +1,13 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 package com.mycompany.minipos;
 
 /**
  *
  * @author OBUZIETTER3000
  */
-public class MiniPOS {
-
-    public static void main(String[] args) {
-        
-        
-        SplashPage sp = new SplashPage();
-        sp.setVisible(true);
-    }
+public class User {
+    
 }
